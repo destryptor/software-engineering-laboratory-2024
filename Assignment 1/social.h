1bc278e6eb39658cc3acaf7eea7a7d970c76fa67
Sharanya Chakraborty <117581936+destryptor@users.noreply.github.com>
2024-01-26 19:53:44 +0530

    Update social.h

--- Assignment 1/social.h
+++ Assignment 1/social.h
@@ -1,152 +1,153 @@
 /*
-    Program Description:
+Name: Sharanya Chakraborty
+Roll No.: 22CS10088
 
-    This program implements a simple social network system with support for individuals, businesses, groups, and organizations. It uses various structures and functions to create, manage, and interact with nodes representing entities in the social network.
+	Program Description:
 
-    Structures:
+	This program implements a simple social network system with support for individuals, businesses, groups, and organizations. It uses various structures and functions to create, manage, and interact with nodes representing entities in the social network.
 
-    1. Node:
-       - Represents a generic node in the social network with essential information such as ID, links to other nodes, name, date, content, and type (individual, business, group, or organization).
+	Structures:
+	1. Node:
+	   - Represents a generic node in the social network with essential information such as ID, links to other nodes, name, date, content, and type (individual, business, group, or organization).
 
-    2. Individual:
-       - Inherits from Node and adds specific information for individuals, such as birthday.
+	2. Individual:
+	   - Inherits from Node and adds specific information for individuals, such as birthday.
 
-    3. Location:
-       - Represents a geographical location with x and y coordinates.
+	3. Location:
+	   - Represents a geographical location with x and y coordinates.
 
-    4. Business:
-       - Inherits from Node and adds information about a business, including its location, owners, and customers.
+	4. Business:
+	   - Inherits from Node and adds information about a business, including its location, owners, and customers.
 
-    5. Group:
-       - Inherits from Node and represents a group of nodes/members.
+	5. Group:
+	   - Inherits from Node and represents a group of nodes/members.
 
-    6. Organisation:
-       - Inherits from Node and represents an organization with a location and members.
+	6. Organisation:
+	   - Inherits from Node and represents an organization with a location and members.
 
-    7. SearchResult:
-       - A structure to store search results, including an array of nodes and the result size.
+	7. SearchResult:
+	   - A structure to store search results, including an array of nodes and the result size.
 
-    Functions:
+	8. Birthday:
+	   - A structure to store birthdays in the format dd, mm, yyyy.
 
-    - Node Creation Functions:
-      - create_node: Creates a new node.
-      - create_individual: Creates a new individual node.
-      - create_business: Creates a new business node.
-      - create_group: Creates a new group node.
-      - create_organisation: Creates a new organization node.
-
-    - Node Modification Functions:
-      - add_member: Adds a member to a group or organization.
-      - add_owner_or_customer: Adds an owner or customer to a business.
-
-    - Node Deletion Functions:
-      - delete_node: Deletes a node and removes its references in the network.
-      - remove_node_from_links: Removes a specific node from the links of another node.
-
-    - Node Search Functions:
-      - search_node_by_name: Searches for nodes by name.
-      - search_node_by_type: Searches for nodes by type.
-      - search_individual_by_birthday: Searches for individuals by birthday.
-
-    - Node Link Management Functions:
-      - is_node_in_links: Checks if a node is already linked to another node.
-
-    - Content and Display Functions:
-      - print_linked_nodes: Prints nodes linked to a specific node.
-      - post_content: Posts content to a node.
-      - search_and_print_content: Searches and prints content of a specific node.
-      - display_linked_content: Displays content linked to a specific node.
-
-    - Utility Functions:
-      - print_node_details: Prints details of a specific node.
-      - print_all_nodes: Prints details of all nodes in the network.
-
-    - User Interface Function:
-      - interface: Implements a user interface for interacting with the social network.
+	ASSUMPTIONS MADE:
+	- The maximum nodes in the network can be 100. This value can be changed by modifying the MAX_NODES macro.
+	- I have used an array to store references of contents posted by nodes, so as to prevent duplication while allowing reposting. The maximum length and number of strings have been set as 100, this can be modified using the MAX_CONTENT macro.
+	- Since the id has been made self incrementing (using global variable id in social.c), most of the functions performing RUD operations ask for the name of the node.
 
 */
 
-#define MAX_NODES 100 // Set as a default value, can be changed as per requirement
+#define MAX_NODES 100	// Set as a default value, can be changed as per requirement
+#define MAX_CONTENT 100 // Set as a default value, can be changed as per requirement
 
 typedef struct Node
 {
-    int id;
-    struct Node **links;
-    int num_links;
-    char *name;
-    char *date; // using the time.h header file to set the date in the format of a string
-    char **content;
-    int num_contents;
-    char type; // I- individual, B- business, G- group, O- organisation
+	int id;
+	struct Node **links;
+	int num_links;
+	char *name;
+	char *date; // using the time.h header file to set the date in the format of a string
+	char **content;
+	int num_contents;
+	char type; // I- individual, B- business, G- group, O- organisation
 } Node;
 
 extern Node *all_nodes[MAX_NODES];
 extern int num_nodes;
 
+extern char all_content[MAX_CONTENT][MAX_CONTENT];
+extern int num_content;
+
+typedef struct Birthday
+{
+	int day;
+	int month;
+	int year;
+} Birthday;
+
 typedef struct Individual
 {
-    Node node;
-    char *birthday;
+	Node node;
+	Birthday birthday;
 } Individual;
 
 typedef struct Location
 {
-    double x;
-    double y;
+	double x;
+	double y;
 } Location;
 
 typedef struct Business
 {
-    Node node;
-    Location location;
-    Individual **owners;
-    int num_owners;
-    Individual **customers;
-    int num_customers;
+	Node node;
+	Location location;
+	Individual **owners;
+	int num_owners;
+	Individual **customers;
+	int num_customers;
 } Business;
 
 typedef struct Group
 {
-    Node node;
-    Node **members;
-    int num_members;
+	Node node;
+	Node **members;
+	int num_members;
 } Group;
 
 typedef struct Organisation
 {
-    Node node;
-    Location location;
-    Individual *members;
-    int num_members;
+	Node node;
+	Location location;
+	Individual *members;
+	int num_members;
 } Organisation;
 
 typedef struct SearchResult
 {
-    Node **nodes;
-    int size;
+	Node **nodes;
+	int size;
 } SearchResult;
 
+// Creates a new node.
 Node *create_node(char *name, char type);
-Individual *create_individual(char *name, char *birthday);
+// Creates a new individual node.
+Individual *create_individual(char *name, Birthday birthday);
+// Creates a new business node.
 Business *create_business(char *name, Location location);
+// Creates a new group node.
 Group *create_group(char *name);
+// Creates a new organisation node.
 Organisation *create_organisation(char *name, Location location);
 
+// Function to add a member to a group or organisation. Groups can have businesses as members too.
 void add_member(Node *group_or_org, Node *new_member);
+// Function to add an owner or customer to a business.
 void add_owner_or_customer(Business *business, Individual *new_owner_or_customer, char role);
 
+// Deletes a node.
 void delete_node(char *name);
+// Utility function to remove links and finish deleting the node.
 void remove_node_from_links(Node *node, Node *target);
+// Search functions for searching by name, type or birthday (birthday, only for individuals)
 SearchResult search_node_by_name(char *name);
 SearchResult search_node_by_type(char type);
-SearchResult search_individual_by_birthday(char *birthday);
+SearchResult search_individual_by_birthday(Birthday birthday);
 
+// Utility function to check if a node is already linked to a node, so that duplicate links aren't created.
 int is_node_in_links(Node *node, Node *target);
+// Prints 1- hop linked nodes.
 void print_linked_nodes(char *name);
+// Function to post content in a node.
 void post_content(char *name, char *content);
+// Function to search by content and print the node which posted that content, allows partial content search too.
 void search_and_print_content(char *name);
+// Displays the contents of individuals linked to an individual.
 void display_linked_content(char *name);
 
+// Prints the details of a node, taking care of special fields for all types.
 void print_node_details(Node *node);
+// Prints all nodes in the network.
 void print_all_nodes();
+// The text-based interface.
 void interface();
