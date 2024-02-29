a4dd1c0dc21900f7d27a99f0da38754caff9b8fc
Sharanya Chakraborty <117581936+destryptor@users.noreply.github.com>
2024-02-29 13:55:04 +0530

    Update VectorDataset.cpp

--- Assignment 3/VectorDataset.cpp
+++ Assignment 3/VectorDataset.cpp
@@ -100,7 +100,10 @@ VectorDataset VectorDataset::knearestneighbor(DataVector a, int k)
     {
         int index = distancesAndIndices[i].second;
         result.set[i] = set[index];
+        cout << i << "th neighbour's index: " << index << endl;
     }
+
+    cout << "-------------------------------------" << endl;
 
     return result;
 }
